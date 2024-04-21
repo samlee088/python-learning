804ca3a119d16a575c0579d592df1cc80f268ee5
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-21 16:16:05 -0700

    strings basic syntax

--- hello-world.py
+++ hello-world.py
@@ -5,3 +5,15 @@ print(message)
 message = "Hello World Updated"
 print(message)
 
+#This is a forced message to view a error message
+# error_message_view = "This is error test"
+# print(eror_message_view)
+
+"""
+Strings
+"""
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
