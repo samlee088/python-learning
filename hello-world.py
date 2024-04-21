e06a01df7adc658ce32e7e288b2fe09a70a5dad9
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-21 16:20:41 -0700

    strings update with spaces and tabs

--- hello-world.py
+++ hello-world.py
@@ -17,3 +17,17 @@ name = "ada lovelace"
 print(name.title())
 print(name.upper())
 print(name.lower())
+
+first_name = "Yugi"
+last_name = "Moto"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+#Spaces and tab spaces
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavascript")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
