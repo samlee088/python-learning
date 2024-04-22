defdefba6f438436e518aea963c61a275ce1d923
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-21 17:59:54 -0700

    new section, sort, reverse sort, reverse order

--- mortorcycles.py
+++ mortorcycles.py
@@ -32,3 +32,21 @@ print(cars)
 
 cars.sort(reverse=True)
 print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars))
+print(cars)
+
+cars.reverse()
+print(cars)
+cars.reverse()
+print(cars)
+
+print(len(cars))
+
+"""
+Working With Lists
+"""
+
+
+
