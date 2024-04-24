1a9352aabd3715b8e46c298cbe2fd9a018dabb37
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-23 20:31:02 -0700

    classes introduction

---
+++ classes.py
@@ -0,0 +1,6 @@
+"""
+classes represent real-world things and situations
+-class define the general behavior that a whole category of objects can have
+-making an object from a class is called instantiation, and work with instances of a class.
+
+"""
