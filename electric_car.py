7901a7e8b7ae7e5a3021d5ed756c711d6e822841
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-24 18:58:05 -0700

    update with electric car information

--- electric_car.py
+++ electric_car.py
@@ -10,8 +10,21 @@ class Car:
     def update_milage(self, milage):
         self.milage += milage
 
+    def print_car_information(self):
+        print(f"car information: {self.make}, {self.model}, {self.year}")
+
 class ElectricCar(Car):
     "A method of creating a child class using Car as the parent class"
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.batterySize = 20
+    
+    def print_battery_information(self): 
+        """Prints out information about the car's battery"""
+        print(f"Battery size is: {self.batterySize}")
+    
+
+my_electric_car = ElectricCar("Toyota","Highlander","2020")
+my_electric_car.print_car_information()
+my_electric_car.print_battery_information()
