30220b05c462937009c92ebe8a0982dd7af92ef1
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-25 06:58:47 -0700

    update with pi million read file

--- file_reader.py
+++ file_reader.py
@@ -9,3 +9,16 @@ contents = path.read_text()
 lines = contents.splitlines()
 for line in lines:
     print(line)
+
+path = Path('pi_million_digits.txt')
+
+line_contents = path.read_text().splitlines()
+
+pi_content = ""
+
+for line in lines:
+    pi_content += line.lstrip()
+
+print(pi_content[:50])
+print(len(pi_content))
+print(pi_content)
