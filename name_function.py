a8f3a31870acf4c178c51428ad83a6069f50dd48
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-25 12:37:53 -0700

    first test case for get_full_formatted_name pass

--- name_function.py
+++ name_function.py
@@ -1,4 +1,4 @@
 def get_formatted_name(first, last):
     """Generate a neatly formatted name"""
-    full_name = (f"{first}{last}")
+    full_name = (f"{first} {last}")
     return full_name.title()
--- test_name_function.py
+++ test_name_function.py
@@ -3,4 +3,4 @@ from name_function import get_formatted_name as gfn
 def test_first_last_name ():
     """Do names like 'Janis Joplin' work?"""
     formatted_name = gfn("Janis", "Joplin")
-    assert formatted_name == "Japnis Joplin"
+    assert formatted_name == "Janis Joplin"
