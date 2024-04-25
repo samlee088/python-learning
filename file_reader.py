79de56eff679e2783fad72cac8b743ce48fb1698
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-25 06:49:48 -0700

    reading files introduction

---
+++ file_reader.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text().rstrip()
+print(contents)
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+lines = contents.splitlines()
+for line in lines:
+    print(line)
